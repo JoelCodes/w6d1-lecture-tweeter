f68414b07a0a45982c19c6b4b5e348d64b0ed9b5
Rob Schwitzer <deadinthewater@me.com>
2018-05-25 05:22:56 +0000

    added database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
 
+///////////display tweets///////////
 function renderTweets(tweets) {
-  $('form').hide();
-  for (var i in tweets) {
+  for (var i in tweets.reverse()) {
     $(".tweets").append(createTweetElement(tweets[i]));
-  }
+  };
 };
 
-renderTweets(createTweetElement);
+///////hide form on refresh////////
+$('form').hide();
 
+////creates html for tweet body////
 function createTweetElement (tweetData) {
-  // console.log(tweetData);
 
-  let tweetBody = tweetData.content.text;
+  let tweetBody     = tweetData.content.text;
   let tweetUserName = tweetData.user.name;
-  let tweetAvatar = tweetData.user.avatars.small;
-  let tweetHandle = tweetData.user.handle;
-  let tweetFooter = tweetData.created_at;
+  let tweetAvatar   = tweetData.user.avatars.small;
+  let tweetHandle   = tweetData.user.handle;
+  let tweetFooter   = tweetData.created_at;
 
-  let $tweet = $('<article>').addClass('all-tweets');
-  let $header = $('<header>').addClass('header');
-  let $footer = $('<footer>').addClass('footer');
-  let $img = $('<img>').addClass('user-img');
-  let $name = $('<h2>').addClass('name');
-  let $handle = $('<h4>').addClass('handle');
-  let $p = $('<p>').addClass('p');
+  let $tweet        = $('<article>').addClass('all-tweets');
+  let $header       = $('<header>').addClass('header');
+  let $footer       = $('<footer>').addClass('footer');
+  let $img          = $('<img>').addClass('user-img');
+  let $name         = $('<h2>').addClass('name');
+  let $handle       = $('<h4>').addClass('handle');
+  let $p            = $('<p>').addClass('p');
 
   $tweet.append($header).append($p).append($footer);
   $header.append($img).append($name).append($handle);
@@ -34,44 +35,40 @@ function createTweetElement (tweetData) {
   $handle.text(tweetHandle);
   $p.text(tweetBody);
   $footer.text(tweetFooter);
+  $footer.attr('src', '/images/flag.png');
 
   return $tweet;
-  };
 
+};
+
+////////////load tweets////////////
 function loadTweets () {
   $.ajax('/tweets/', {method: 'GET'}).done(function(tweets) {
   $(".tweets").empty();
-  for (var i in tweets) {
-    $(".tweets").prepend(createTweetElement(tweets[i]));
-  }
-
-  // let html = createTweetElement(tweet);
-  // $('.container').append(html);
+  renderTweets(tweets);
   $('input[name="text"]').text('');
-  })
-}
+  });
+};
 
+///////toggle compose button///////
 $('.compose').on('click', function(button) {
   $("form").slideToggle('slow', function(){
-        $("textarea").focus();
-    });
-})
+    $("textarea").focus();
+  });
+});
 
+////////////post tweet////////////
 $('form').on('submit', function(event) {
   event.preventDefault();
   let data = $(this).serialize();
-  let tweetLength = event.currentTarget[0].value.length;
-  // console.log();
-  // if (tweetLength > 140 || tweetLength === undefined) {
-
-  // }
   $.ajax('/tweets/', {method: 'POST', data: data}).done(function (tweet) {
-  loadTweets();
-  });
+    loadTweets();
+    });
   $('textarea').val('');
   $('.counter').text('140');
-})
+  });
 
+loadTweets();
 
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,9 +4,6 @@ $(document).ready(function() {
   tweet.on('input', function(event) {
     const remainingLength = 140 - event.target.value.length;
     counter.text(remainingLength);
-    if (tweet.length === 0) {
-      // console.log(tweet.length);
-    }
     if (remainingLength < 10) {
       $(counter).css({color: 'red'});
     } else {
